d4403448c8f1e860335663d8c7271e442ab332b4
Natt Srifah <srifah.n@gmail.com>
2022-12-16 08:52:59 -0800

    First commit

---
+++ src/main/java/com/jenkins/controller/GreetingController.java
@@ -0,0 +1,14 @@
+package com.jenkins.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingController {
+	@GetMapping("/")
+	public String sayHello(Model model) {
+		model.addAttribute("message", "Hello Jenkins");
+		return "greeting";
+	}
+}
